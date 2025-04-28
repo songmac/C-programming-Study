fcc1a37b723cc8a5e8791176872659705b8f5e63
songmac <ssy4784@naver.com>
2025-04-28 22:46:26 +0900

    Update : array for 3-dim

--- game7/m_array.c
+++ game7/m_array.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
     // 다차원 배열 Multidimensional Array
-    int i; // □ 
-    int arr[5]; // □□□□□
-    int arr2[2][5]; // □□□□□
-                    // □□□□□
+    int i; 
+    // □ 
 
+    int arr[5]; // 1*5
+    // □□□□□
+
+    int arr2[2][5]; // 2*5
+    // □□□□□
+    // □□□□□
+
+    int arr3[4][3]; // 4*3
+    // □□□□
+    // □□□□
+    // □□□□
+
+    int arr4[3][3][3]; // 3*3*3
+    // □□□
+    // □□□
+    // □□□
+
+    // □□□
+    // □□□
+    // □□□
+    
+    // □□□
+    // □□□
+    // □□□
+    
     return 0;
 }

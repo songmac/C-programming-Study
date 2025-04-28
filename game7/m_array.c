19c605b01463c42ec10a6661799838715f56bef0
songmac <ssy4784@naver.com>
2025-04-28 22:29:25 +0900

    Update : add basic array by 2-dim

---
+++ game7/m_array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    // 다차원 배열 Multidimensional Array
+    int i; // □ 
+    int arr[5]; // □□□□□
+    int arr2[2][5]; // □□□□□
+                    // □□□□□
+
+    return 0;
+}

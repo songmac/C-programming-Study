45f79bec7dc9312af1af14731046633136d36d6b
songmac <ssy4784@naver.com>
2025-05-01 00:00:45 +0900

    Update : add functions of getEmptyPosition, conv_pos_x, conv_pos_y

--- game7/m_array_proeject.c
+++ game7/m_array_proeject.c
@@ -5,16 +5,15 @@
 // 모든 동물 쌍을 찾으면 게임 종료
 // 총 실패 횟수 알려주기
 
-
 int arrayAnimal[4][5]; // 카드 지도(20장)
-char * strAnimal[10]; // 동물 이름
+char *strAnimal[10];   // 동물 이름
 
 void initAnimalArray();
 void initAnimalName();
 void shuffleAnimal();
 int getEmptyPosition();
-void conv_pos_x();
-void conv_pos_y();
+int conv_pos_x(int x);
+int conv_pos_y(int y);
 
 int main(void)
 {
@@ -23,15 +22,18 @@ int main(void)
     initialArray();
     initAnimalName();
     shuffleAnimal();
+    int getEmptyPosition();
+    int conv_pos_x(int x);
+    int conv_pos_y(int y);
 
     return 0;
-}    
+}
 
 void initAnimalArray()
 {
     for (int i; i < 4; i++)
     {
-        for (int j; j<4; j++)
+        for (int j; j < 4; j++)
         {
             arrayAnimal[i][j] = -1; // 우선 -1로 초기화
         }
@@ -58,7 +60,7 @@ void shuffleAnimal()
     // ㅁㅁㅁㅁㅁ
     // ㅁㅁㅁㅁㅁ
     // ㅁㅁㅁㅁㅁ
-    
+
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 2; j++)
@@ -74,15 +76,34 @@ void shuffleAnimal()
 
 int getEmptyPosition() // 좌표에서 빈 공간 찾기
 {
-    while(1)
+    // ㅁㅁㅁㅁㅁ  0  1  2  3  4
+    // ㅁㅁㅁㅁㅁ  5  6  7  8  9
+    // ㅁㅁㅁㅁㅁ  10 11 12 13 14
+    // ㅁㅁㅁㅁㅁ  15 16 17 18 19
+    while (1)
+    {
+        int randPos = rand() % 20; // 0~19 사이의 숫자 반환
+        // 19 -> (3, 4)
+        int x = conv_pos_x(randPos); // 0 ~ 3 (추후 5로 나눈 몫의 값)
+        int y = conv_pos_y(randPos); // 1 ~ 4 (추후 5로 나눈 나머지 값)
+
+        if (arrayAnimal[x][y] == -1) // 카드 지도에 값이 비어있으면 실행
+                                     // ->값이 비어있지 않으면, 동물 이름에 맞는 번호가 들어간 상태
+        {
+            return randPos;
+        }
+    }
+    return 0;
 }
 
-void conv_pos_x()
+int conv_pos_x(int x)
 {
-
+    // 19 -> (3, 4)
+    return x / 5; // 0 ~ 3 (규칙 : 5로 나눈 몫의 값)
 }
 
-void conv_pos_y()
+int conv_pos_y(int y)
 {
-
+    // 19 -> (3, 4)
+    return y % 5; // 1 ~ 4 (규칙 : 5로 나눈 나머지 값)
 }

14c56b587e7415f9aedd56f7482c6bb565b54b6b
songmac <ssy4784@naver.com>
2025-05-04 00:21:20 +0900

    Update : revise struct format from basic to array

--- game8/struct.c
+++ game8/struct.c
@@ -38,16 +38,11 @@ int main(void)
 
     // 구조체 사용
     struct GameInfo gameInfo1; // struct GamdInfo 까지 자료형, 그 이후가 변수명임
-    struct GameInfo gameInfo2;
+    // struct GameInfo gameInfo2;
     gameInfo1.name = "나도게임";
     gameInfo1.year = 2017;
     gameInfo1.price = 50;
     gameInfo1.company = "나도회사";
-
-    gameInfo2.name = "너도게임";
-    gameInfo2.year = 2017;
-    gameInfo2.price = 100;
-    gameInfo2.company = "너도회사";
 
     // 구조체 출력
     printf("-- 게임 출시 정보 --\n");
@@ -56,6 +51,8 @@ int main(void)
     printf("가격 : %d\n", gameInfo1.price);
     printf("제작사 : %s\n", gameInfo1.company);
 
+    // 구조체를 배열처럼 초기화
+    struct GameInfo gameInfo2 = {"너도게임", 2017, 100, "너도회사"};
     printf("-- 또다른 게임 출시 정보 --\n");
     printf("게임명 : %s\n", gameInfo2.name);
     printf("발매년도 : %d\n", gameInfo2.year);

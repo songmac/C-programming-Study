e822c76cd73326c3afd25e6b96bac2f5ce6cf915
songmac <ssy4784@naver.com>
2025-05-03 23:46:19 +0900

    Update : add information of a virtual game company

---
+++ game8/struct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // [게임 출시]
+    // 이름 : 나도 게임
+    // 발매년도 : 2017 년
+    // 가격 : 50원
+    // 제작사 : 나도회사
+
+    char * name = "나도게임";
+    int year = 2017;
+    int price = 50;
+    char * company = "나도회사";
+
+    return 0;
+}

1c0f391b0da0ea46b267d62b6a0d21965b88d062
songmac <ssy4784@naver.com>
2025-05-04 23:01:54 +0900

    Update : different format of a struct type of pointer

--- game8/struct.c
+++ game8/struct.c
@@ -55,7 +55,7 @@ int main(void)
     // 구조체를 배열처럼 초기화
     struct GameInfo gameInfo2 = {"너도게임", 2017, 100, "너도회사"};
     printf("-- 또다른 게임 출시 정보 --\n");
-    printf("게임명 : %s\n", gameInfo2.name);
+    printf("게임명 : %s\n", gameInfo2.name); // 구조체의 변수에 접근하기 위해 '.' 을 사용
     printf("발매년도 : %d\n", gameInfo2.year);
     printf("가격 : %d\n", gameInfo2.price);
     printf("제작사 : %s\n", gameInfo2.company);
@@ -70,11 +70,17 @@ int main(void)
     struct GameInfo * gamePtr; // 미션맨
     gamePtr = &gameInfo1;
     printf("-- 미션맨의 게임 출시 정보 --\n");
-    printf("게임명 : %s\n", (*gamePtr).name);
+    /*
+    printf("게임명 : %s\n", (*gamePtr).name); // 포인터가 가리키는 값의 정보에 접근하기 때문에 (*gamePtr) 이라 작성함
     printf("발매년도 : %d\n", (*gamePtr).year);
     printf("가격 : %d\n", (*gamePtr).price);
     printf("제작사 : %s\n", (*gamePtr).company);
-
+    */
+    // 조금 더 간단한 표현
+    printf("게임명 : %s\n", gamePtr->name); // 구조체 포인터에 접근하기 위해 '->' 를 사용
+    printf("발매년도 : %d\n", gamePtr->year);
+    printf("가격 : %d\n", gamePtr->price);
+    printf("제작사 : %s\n", gamePtr->company);
 
     return 0;
 }

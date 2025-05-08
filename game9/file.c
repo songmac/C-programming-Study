1b944b9f871e7e049b83cd7671fd1e8d7ff14bc2
songmac <ssy4784@naver.com>
2025-05-08 22:40:37 +0900

    Upload : new file for learning file system

---
+++ game9/file.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+# define MAX 10000
+
+int main(void)
+{
+    // 파일 입출력
+    // 파일에 어떤 데이터를 저장 
+    // 사용하는 이유? 프로그램을 껐다 키면 메모리에 저장된 데이터는 휘발성이기 때문에 날아가지만, 파일로 저장하면 데이터가 남아있게 되므로, 데이터 저장시 유용
+    // 파일에 저장된 데이터를 불러오기
+
+
+    // fputs, fgets 쌍
+    char line[MAX];
+
+
+
+    // fprintf, fscanf 쌍
+
+    return 0;
+}
